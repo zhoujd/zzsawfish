7c826312ba94545bf010fc88d1f6c4a3780500fe
zhoujd <zjd-405@163.com>
2012-09-08 21:42:43 +0800

    for sawfish-pager

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo -n "Do you need install packages? (y/N): "
 read answer
 case "$answer" in
     "Y" | "y" )
-        sudo apt-get install -y sawfish sawfish-data sawfish-dbg sawfish-lisp-source sawfish-merlin-ugliness sawfish-themes
+        sudo apt-get install -y sawfish sawfish-data sawfish-dbg sawfish-lisp-source sawfish-merlin-ugliness sawfish-themes sawfish-pager
         sudo apt-get install -y gnome-panel xscreensaver gmrun gcolor2 gcalctool libimlib2 
         sudo apt-get install -y numlockx  imagemagick remmina geany gtkorphan gprename rcconf
         sudo apt-get install -y feh fbpanel 
