45dfb05f26f8637634450d4d80c3093f2b5459b3
zhoujd <jiandongx.zhou@intel.com>
2012-09-24 08:31:04 +0800

    for change

--- configure/common-setting.jl
+++ configure/common-setting.jl
@@ -29,7 +29,7 @@
 (custom-set-typed-variable (quote wm-modifier-value) (quote (super)) (quote modifier-list))
 
 ;;wallpaper setting
-(setq list-wallpaper '("black.png" "sand.png"   "grass.jpg"  "grid.jpg"
+(setq list-wallpaper '("black.png" "sand.jpg"   "grass.jpg"  "grid.jpg"
                        "red.jpg"   "summer.jpg" "yellow.jpg" "moon.jpg"))
 
 ;;set start program
@@ -48,7 +48,7 @@
     '("numlockx" "on")
 
     ;;wallpaper setting
-    (list "feh" "--bg-fill" (concat zzsawfish-path "wallpaper/" (nth 5 list-wallpaper)))
+    (list "feh" "--bg-fill" (concat zzsawfish-path "wallpaper/" (nth 3 list-wallpaper)))
     ;;system info show
     (list "conky" (format nil "-x %d" (- (screen-width) 400)) (format nil "-y %d" (- (screen-height) 600)))
     ))
